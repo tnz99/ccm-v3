a51ed2662241945264842bc3427c58ecd0b4604c
Tenzin Tshomo <12200090.gcit@rub.edu.bt>
2024-05-15 22:38:00 +0600

    news and events

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -1,3 +1,7 @@
 <x-page-layout  backgroundImageName="images/about.png" :darkMode="false" cnavBackground="linear-gradient(270deg, #99992a 42.11%, rgba(188, 219, 240, 0.40) 101.31%)">
-    About
+    <div class="mt-12">
+        <h2 class="text-6xl font-[900]">About</h2>
+        <h2 class="text-6xl font-[900]">Us</h2>
+        <p class="w-6/12 mt-10">The Climate Change Museum in Bhutan inspires global action by raising awareness and promoting sustainability. Explore exhibits, access educational resources, and join us in the fight against climate change for a sustainable future.</p>
+    </div>
 </x-page-layout>
