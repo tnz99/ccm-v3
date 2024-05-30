f7a581b17473d5e6f7276c5f14de7d7941a0a828
Tenzin Tshomo <12200090.gcit@rub.edu.bt>
2024-05-31 01:02:29 +0600

    deploy 8

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 # Run migrations and seed the database
-php artisan migrate --force
-php artisan db:seed --force
+php artisan db:wipe
+php artisan migrate
+php artisan db:seed
 
 # Start Apache
 exec apache2-foreground
