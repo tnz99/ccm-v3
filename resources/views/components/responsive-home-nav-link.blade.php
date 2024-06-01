ea413b9d35d1b1e9959517ad66e787e6a532d23a
Tenzin Tshomo <12200090.gcit@rub.edu.bt>
2024-06-02 00:48:53 +0600

    header nav

--- resources/views/components/responsive-home-nav-link.blade.php
+++ resources/views/components/responsive-home-nav-link.blade.php
@@ -2,13 +2,13 @@
 
 @php
 $classes = ($active ?? false)
-            ? 'font-semibold uppercase text-xs bg-[#FCFF5C] text-black p-2 px-5 xl:flex xl:items-center xl:justify-center text-black'
+            ? 'font-semibold uppercase text-xs bg-[#FCFF5C] text-black p-2 px-5 xl:flex xl:items-center xl:justify-center'
             : 'hover:font-semibold uppercase text-xs  hover:bg-[#FCFF5C] hover:text-black  p-2 px-5 xl:flex xl:items-center xl:justify-center';
 
 $darkMode = ($darkMode?? false) ? ' text-white' : ' text-black';
 
 if($darkMode && $active) {
-    $classes = $classes;
+    $classes = $classes.'text-black';
 } else {
     $classes = $classes.$darkMode;
 }
