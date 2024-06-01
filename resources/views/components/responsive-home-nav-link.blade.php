b626a0c1c063840e7cb9b9cdf96d9443870139ac
Tenzin Tshomo <12200090.gcit@rub.edu.bt>
2024-06-01 16:31:51 +0600

    add flex

--- resources/views/components/responsive-home-nav-link.blade.php
+++ resources/views/components/responsive-home-nav-link.blade.php
@@ -2,8 +2,8 @@
 
 @php
 $classes = ($active ?? false)
-            ? 'font-semibold uppercase text-xs bg-yellow-300 p-2 px-5'
-            : 'hover:font-semibold uppercase text-xs  hover:bg-yellow-300 p-2 px-5';
+            ? 'font-semibold uppercase text-xs bg-yellow-300 p-2 px-5 xl:flex xl:items-center xl:justify-center'
+            : 'hover:font-semibold uppercase text-xs  hover:bg-yellow-300 p-2 px-5 xl:flex xl:items-center xl:justify-center';
 
 $darkMode = ($darkMode?? false) ? ' text-white' : ' text-black';
 
